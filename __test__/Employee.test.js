403902ca9dfae7b2953bff72b22255f6dc7036e3
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-09-21 19:20:41 -0500

    final commit

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,11 +1,13 @@
 const { default: test } = require('node:test')
 const { hasUncaughtExceptionCaptureCallback } = require('process')
+const Employee = require('../lib/Employee')
 const employee = require('../lib/Employee')
+const newEmployee = new Employee('Mark', 01234567, 'mark@gmail.com');
 
 test('creates an employee name',() => {
-    const employee = new employee('Jared')
+    expect(newEmployee.name).toBe('Mark')
+    expect(newEmployee.id).toBe(01234567);
+    expect(newEmployee.email).toBe("mark@gmail.com")
 
-    expect(employee.name).toEqual(
-        expect.arrayContaining([expect.any(name)])
-    );
-})
+});
+test('run test for name of object', () =>)
--- index.js
+++ index.js
@@ -1,4 +1,109 @@
-const intern = require('./lib/Intern')
-const engineer = require('./lib/Engineer')
-const manager = require('./lib/Manager')
-const employee = require('./lib/Employee')
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
+
+const promptUser = () => {
+  return inquirer.prompt([
+    
+  ]);
+};
+
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Employee
+=================
+`);
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Provide the email of new employee (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('You need to enter a email!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'list',
+        name: 'position',
+        message: 'What is the position of the employ',
+        choices: ['Manager', 'Intern', 'Engineer']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link for the new employee. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this employee?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter a new employee?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
